144ac336b88cb87f2d9b7e0e3b53f11488de234c
Kenny CALVAT <kenny.calvat@gmail.com>
2015-10-03 09:31:37 +0200

    Refactoring du code

--- post-install.sh
+++ post-install.sh
@@ -19,6 +19,34 @@
 # Les variables
 # --------------------------------------------------------------
 
+# Chemin sources dans le dossier d'installation
+path_src_conf_apache='./conf/apache'
+path_src_conf_moodle='./conf/moodle'
+path_src_conf_php='./conf/php'
+path_src_conf_ssmtp='./conf/ssmtp'
+path_src_conf_system='./conf/system'
+
+# Chemin distant sur le serveur
+path_dst_conf_apache='/etc/apache2/sites-available'
+
+# APT source files
+file_src_apt_source="$path_src_conf_system/sources.list" 
+file_dst_apt_source='/etc/apt/sources.list'
+
+# VIM config files
+file_src_vim_conf="$path_src_conf_system/.vimrc"
+file_dst_vim_conf="$HOME/.vimrc"
+
+# SSMTP Config files
+file_src_ssmtp_conf="$path_src_conf_ssmtp/ssmtp.conf"
+file_dst_ssmtp_conf='/etc/ssmtp/ssmtp.conf'
+file_src_ssmtp_revaliases="$path_src_conf_ssmtp/revaliases"
+file_dst_ssmtp_revaliases='/etc/ssmtp/revaliases'
+
+# Apticron config
+file_dst_apticron_conf='/etc/apticron/apticron.conf'
+file_dst_unattended_upggrades='/etc/apt/apt.conf.d/50unattended-upgrades'
+
 # Les chemins
 fichierSources='/etc/apt/sources.list'
 fichierLog='log/postinstall.log'
@@ -129,15 +157,15 @@ cecho "Utilitaire de configuration" yellow
 cecho "---------------------------------------------------------" yellow
 echo
 
-# Adresse email du demandeur
+# -------------------- Adresse email du demandeur --------------------
 	echo -n "Entrer votre adresse e-mail (les informations de connexions vous seront envoyées) : "
 	read email_demandeur
 
-# URL du Moodle à installer
+# -------------------- URL du Moodle à installer --------------------
 	echo -n "Entrer l'URL du Moodle : "
 	read urldusite
 
-# Choix du protocole HTTP
+# -------------------- Choix du protocole HTTP --------------------
 	echo "Quel protocole Souhaitez-vous utiliser ? "
 	echo "  1. HTTP seul"
 	echo "  2. HTTPS seul"
@@ -161,35 +189,35 @@ echo
 
 	apt-get install -y pwgen > /dev/null
 
-# Définition du compte
-	compte_moodle=`echo $urldusite | cut -f1 -d.`
-	compte_moodle_mdp=`pwgen -1`
-	compte_db_moodle_mdp=`pwgen -1`
-	compte_db_root_mdp=`pwgen -1`
+# -------------------- Définition du compte --------------------
+	account_system_moodle_name=`echo $urldusite | cut -f1 -d.`
+	account_system_moodle_password=`pwgen -1`
+	account_db_moodle_password=`pwgen -1`
+	account_db_root_password=`pwgen -1`
 
-# Définition du dossier racine moodle
-	dossier_moodle_racine="/var/www/$compte_moodle"
-	dossier_moodle_systeme="$dossier_moodle_racine/moodle"
-    dossier_moodledata="$dossier_moodle_racine/moodledata"
+# -------------------- Définition du dossier racine moodle --------------------
+	folder_moodle_root="/var/www/$account_system_moodle_name"
+	folder_moodle_system="$folder_moodle_root/moodle"
+    folder_moodle_data="$folder_moodle_root/moodledata"
 
-# Affichage des variables à l'écran pour contrôle
+# -------------------- Affichage des variables à l'écran pour contrôle --------------------
 	echo "Ceci va définir les variables de scripts suivants :"
 	echo "- Email du demandeur : $email_demandeur"
 	echo "- URL du vHost : $urldusite"
 	echo "- Protocole utilisé : $protocole"
-	echo "- Compte système Moodle : $compte_moodle"
-	echo "- Mot de passe du compte système Moodle : $compte_moodle_mdp"
-	echo "- Dossier d'installation racine : $dossier_moodle_racine"
-	echo "- Dossier système Moodle : $dossier_moodle_systeme"
-	echo "- Dossier data Moodle : $dossier_moodledata"
+	echo "- Compte système Moodle : $account_system_moodle_name"
+	echo "- Mot de passe du compte système Moodle : $account_system_moodle_password"
+	echo "- Dossier d'installation racine : $folder_moodle_root"
+	echo "- Dossier système Moodle : $folder_moodle_system"
+	echo "- Dossier data Moodle : $folder_moodle_data"
 	echo "- Compte root base de données : root"
-	echo "- Mot de passe du compte root base de données : $compte_db_root_mdp"
-	echo "- Nom de la base de données : $compte_moodle"
-	echo "- Utilisateur de la base de donnée : $compte_moodle"
-	echo "- Mot de passe de l'utilisateur base de donnée : $compte_db_moodle_mdp"
+	echo "- Mot de passe du compte root base de données : $account_db_root_password"
+	echo "- Nom de la base de données : $account_system_moodle_name"
+	echo "- Utilisateur de la base de donnée : $account_system_moodle_name"
+	echo "- Mot de passe de l'utilisateur base de donnée : $account_db_moodle_password"
 	echo
 
-# Confirmation des variables par l'utilisateur
+# -------------------- Confirmation des variables par l'utilisateur --------------------
 	echo "Souhaitez-vous continuer ? [o/n] "
 	read choix
 
@@ -204,25 +232,15 @@ echo
 	cecho "---------------------------------------------------------" yellow
 	echo
 
-# Sauvegarde le fichier courant des sources. Le numéro final le plus grand et le fichier le plus récent.
-	if ! [ -f /etc/apt/sources.list.old.1 ] ; then
-		cp $fichierSources /etc/apt/sources.list.old.1
-	else
-		numero=`ls /etc/apt/sources* | sort -r | head -1 | cut -d. -f4`
-		numero=$(($numero + 1))
-
-		cp $fichierSources /etc/apt/sources.list.old.$numero
-	fi
-
-# Modification du fichier des sources
+# -------------------- Modification du fichier des sources --------------------
 	echo -n "- Modification du fichier sources.list : "
-	if cat conf/sources.list > /etc/apt/sources.list ; then
+	if cat $file_src_apt_source > $file_dst_apt_source ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
 
-# Mise à jour du système
+# -------------------- Mise à jour du système --------------------
 	echo -n "- Mise à jour du Système : "
 	if apt-get update > /dev/null && apt-get upgrade -y > /dev/null && apt-get check > /dev/null && apt-get autoclean > /dev/null && apt-get autoremove > /dev/null ; then
 		cecho "[OK]" green
@@ -230,11 +248,11 @@ echo
 		cecho "[BAD]" red
 	fi
 
-# Configuration de MySQL avant installation
-	debconf-set-selections <<< "mysql-server mysql-server/root_password password $compte_db_root_mdp"
-	debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $compte_db_root_mdp"
+# -------------------- Configuration de MySQL avant installation --------------------
+	debconf-set-selections <<< "mysql-server mysql-server/root_password password $account_db_root_password"
+	debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $account_db_root_password"
 
-# Installation des paquets de base
+# -------------------- Installation des paquets de base --------------------
 	echo
 	cecho "Installation des paquets :" yellow
 	for paquet in $liste_paquets ; do
@@ -249,94 +267,86 @@ echo
 	echo
 	cecho "Configuration des paquets :" yellow
 
-# Configuration de VIM
+# -------------------- Configuration de VIM --------------------
 	echo -n "- Configuration de VIM : "
-	if cat conf/.vimrc > $HOME/.vimrc ; then
+	if cat $file_src_vim_conf > $file_dst_vim_conf ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
 
-# Configuration de SSMTP
+# -------------------- Configuration de SSMTP --------------------
 	echo -n "- Configuration de SSMTP : "
-	if cat conf/ssmtp.conf > /etc/ssmtp/ssmtp.conf ; then
+	if cat $file_src_ssmtp_conf > $file_dst_ssmtp_conf ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
 
-	hostname=`hostname`
-	sed -i -e 's/nom_hote/'"$hostname"'/' /etc/ssmtp/ssmtp.conf
+	sed -i -e 's/nom_hote/`hostname`/' $file_dst_ssmtp_conf
 
 	echo -n "- Configuration de revaliases : "
-	if cat conf/revaliases > /etc/ssmtp/revaliases ; then
+	if cat $file_src_ssmtp_revaliases > $file_dst_ssmtp_revaliases ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
-
-# Test d'envoie d'e-mail
-	echo `hostname` : Mail ok ! | mail -s "test d'envoi d'email depuis `hostname`" $email_demandeur
 
 	echo
 	cecho "Compte et arborescence Moodle :" yellow
 
-# Configuration de apticron
-	sed -i -e 's/EMAIL="root"/EMAIL="'"$email_demandeur"'"/' /etc/apticron/apticron.conf
-	sed -i -e 's/# SYSTEM="foobar.example.com"/SYSTEM="'"$urldusite - `hostname`"'"/' /etc/apticron/apticron.conf
+# -------------------- Configuration de apticron --------------------
+	sed -i -e 's/EMAIL="root"/EMAIL="'"$email_demandeur"'"/' $file_dst_apticron_conf
+	sed -i -e 's/# SYSTEM="foobar.example.com"/SYSTEM="'"$urldusite - `hostname`"'"/' $file_dst_apticron_conf
 
-# Configuration de unattended-upgrade
-	sed -i -e 's/Unattended-Upgrade::Mail "root";/Unattended-Upgrade::Mail "'"$email_demandeur"'";/' /etc/apt/apt.conf.d/50unattended-upgrades
+# -------------------- Configuration de unattended-upgrade --------------------
+	sed -i -e 's/Unattended-Upgrade::Mail "root";/Unattended-Upgrade::Mail "'"$email_demandeur"'";/' $file_dst_unattended_upggrades
 
-# Création de l'utilisateur Moodle
-	echo -n "- Création du compte $compte_moodle : "
-	if useradd $compte_moodle ; then
+# -------------------- Création de l'utilisateur Moodle --------------------
+	echo -n "- Création du compte $account_system_moodle_name : "
+	if useradd $account_system_moodle_name ; then
 		cecho "[OK]" green
-		echo "$compte_moodle:$compte_moodle_mdp" | chpasswd
+		echo "$account_system_moodle_name:$account_system_moodle_password" | chpasswd
 	else
 		cecho "[BAD]" red
 	fi
 
-# Création de l'arborescence
-	echo -n "- Création du dossier $dossier_moodle_systeme : "
-	if ! [ -d $dossier_moodle_systeme ] ; then
-		mkdir -p $dossier_moodle_systeme
+# -------------------- Création de l'arborescence --------------------
+	echo -n "- Création du dossier $folder_moodle_system : "
+	if mkdir -p $folder_moodle_system ; then	
 		cecho "[OK]" green
 	else
 		cecho "[OK]" green
 	fi
 	
-	echo -n "- Création du dossier $dossier_moodledata : "
-	if ! [ -d $dossier_moodledata ] ; then
-		mkdir -p $dossier_moodledata
+	echo -n "- Création du dossier $folder_moodle_data : "
+	if mkdir -p $folder_moodle_data ; then
 		cecho "[OK]" green
 	else
 		cecho "[OK]" green
 	fi
 
-# Application des droits aux dossiers
-	echo -n "- Changement des droits du dossier $dossier_moodledata : "
-	if chmod 777 $dossier_moodledata ; then
+	echo -n "- Changement des droits du dossier $folder_moodle_data : "
+	if chmod 777 $folder_moodle_data ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
 
-# Changement des propriétaires des dossiers
-	echo -n "- Changement des propriétaires des dossiers $dossier_moodle_racine : "
-	if chown -R www-data: $dossier_moodle_racine/* ; then
+	echo -n "- Changement des propriétaires des dossiers $folder_moodle_root : "
+	if chown -R www-data: $folder_moodle_root/* ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
 	fi
 
-# Installation de la source Moodle
+# -------------------- Installation des sources Moodle --------------------
 	if [ -f ./moodle-*.tgz ] ; then
 		echo
 		cecho "Installation des sources depuis `ls moodle-*.tgz` :" yellow
 		tar xfz ./moodle-*.tgz
 
-		if mv moodle/* $dossier_moodle_systeme ; then
+		if mv moodle/* $folder_moodle_system ; then
 			cecho "[OK]" green
 		else
 			cecho "[BAD]" red
@@ -344,7 +354,7 @@ echo
 	else
 		echo
 		cecho "Récupération des sources github :" yellow
-		if git clone https://github.com/moodle/moodle.git $dossier_moodle_systeme ; then
+		if git clone https://github.com/moodle/moodle.git $folder_moodle_system ; then
 			cecho "[OK]" green
 		else
 			cecho "[BAD]" red
@@ -355,10 +365,10 @@ echo
 	cecho "Configuration de Apache et php5-fpm :" yellow
 
 # Configuration du vHost Apache
-	cp conf/moodle-http.conf /etc/apache2/sites-available/http-$urldusite.conf
+	cp $path_conf_apache/moodle-http.conf /etc/apache2/sites-available/http-$urldusite.conf
 	sed -i -e 's/email_demandeur/'"$email_demandeur"'/' /etc/apache2/sites-available/http-$urldusite.conf
 	sed -i -e 's/urldusite/'"$urldusite"'/' /etc/apache2/sites-available/http-$urldusite.conf
-	sed -i -e 's/dossier_moodle_racine/'"$compte_moodle"'/' /etc/apache2/sites-available/http-$urldusite.conf
+	sed -i -e 's/folder_moodle_root/'"$account_system_moodle_name"'/' /etc/apache2/sites-available/http-$urldusite.conf
 	
 # Autorisation du vHost et rechargement de la configuration apache
 	if [ -f /etc/apache2/sites-enabled/*default* ]; then
@@ -381,9 +391,9 @@ echo
 	service apache2 restart > /dev/null
 
 # Création de la base de données et attribution des droits
-	mysql -u root -p"$compte_db_root_mdp" -e "CREATE DATABASE $compte_moodle;"
-	echo "GRANT ALL PRIVILEGES ON $compte_moodle.* TO $compte_moodle@'%' IDENTIFIED BY '$compte_db_moodle_mdp';" > ./compte_moodle.sql
-	mysql -u root -p"$compte_db_root_mdp" < ./compte_moodle.sql
+	mysql -u root -p"$account_db_root_password" -e "CREATE DATABASE $account_system_moodle_name;"
+	echo "GRANT ALL PRIVILEGES ON $account_system_moodle_name.* TO $account_system_moodle_name@'%' IDENTIFIED BY '$account_db_moodle_password';" > ./account_system_moodle_name.sql
+	mysql -u root -p"$account_db_root_password" < ./account_system_moodle_name.sql
 
 # Configuration de l'upload php5-fpm
 	sed -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 256M/' /etc/php5/fpm/php.ini
@@ -399,11 +409,11 @@ echo
 	service php5-fpm restart > /dev/null
 
 # Installation automatique de Moodle
-	php $dossier_moodle_systeme/admin/cli/install.php --allow-unstable --non-interactive --lang=fr --wwwroot=http://$urldusite --dataroot=$dossier_moodledata --dbname=$compte_moodle --dbuser=$compte_moodle --dbpass=$compte_db_moodle_mdp --fullname=$compte_moodle --shortname=$compte_moodle --adminuser=admin_symetrix --adminpass=symetrix --adminemail=$email_demandeur --agree-license
+	php $folder_moodle_system/admin/cli/install.php --allow-unstable --non-interactive --lang=fr --wwwroot=http://$urldusite --dataroot=$folder_moodle_data --dbname=$account_system_moodle_name --dbuser=$account_system_moodle_name --dbpass=$account_db_moodle_password --fullname=$account_system_moodle_name --shortname=$account_system_moodle_name --adminuser=admin_symetrix --adminpass=symetrix --adminemail=$email_demandeur --agree-license
 
 # Changement des propriétaires des dossiers
-	echo -n "- Changement des propriétaires des dossiers $dossier_moodle_racine : "
-	if chown -R www-data: $dossier_moodle_racine/* ; then
+	echo -n "- Changement des propriétaires des dossiers $folder_moodle_root : "
+	if chown -R www-data: $folder_moodle_root/* ; then
 		cecho "[OK]" green
 	else
 		cecho "[BAD]" red
@@ -442,41 +452,41 @@ echo
 	fi
 
 # Configuration du cron
-	echo "*/15 * * * * /usr/bin/php $dossier_moodle_systeme/admin/cli/cron.php > /dev/null" > /var/spool/cron/crontabs/www-data
+	echo "*/15 * * * * /usr/bin/php $folder_moodle_system/admin/cli/cron.php > /dev/null" > /var/spool/cron/crontabs/www-data
 	chown www-data:crontab /var/spool/cron/crontabs/www-data
 	chmod 600 /var/spool/cron/crontabs/www-data
 
 # Configuration de forcelogin
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='1' WHERE name='forcelogin'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='1' WHERE name='forcelogin'"
 
 # Configuration de CLAMAV
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='1' WHERE name='runclamonupload'"
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='/usr/bin/clamscan' WHERE name='pathtoclam'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='1' WHERE name='runclamonupload'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='/usr/bin/clamscan' WHERE name='pathtoclam'"
 
 # Configuration de DU
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='/usr/bin/du' WHERE name='pathtodu'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='/usr/bin/du' WHERE name='pathtodu'"
 
 # Configuration SMTP dans Moodle
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='smtp.gmail.com:587' WHERE name='smtphosts'" 
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='tls' WHERE name='smtpsecure'" 
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='no-reply@symetrix.fr' WHERE name='smtpuser'"
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='N0ReplySym' WHERE name='smtppass'"
-	mysql -u $compte_moodle -p$compte_db_moodle_mdp $compte_moodle -e "UPDATE mdl_config SET value='10' WHERE name='smtpmaxbulk'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='smtp.gmail.com:587' WHERE name='smtphosts'" 
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='tls' WHERE name='smtpsecure'" 
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='no-reply@symetrix.fr' WHERE name='smtpuser'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='N0ReplySym' WHERE name='smtppass'"
+	mysql -u $account_system_moodle_name -p$account_db_moodle_password $account_system_moodle_name -e "UPDATE mdl_config SET value='10' WHERE name='smtpmaxbulk'"
 
 # Envoie des informations par email
 	body="- Email du demandeur : $email_demandeur
 	- URL du vHost : $urldusite
 	- Protocole utilisé : $protocole
-	- Compte système Moodle : $compte_moodle
-	- Mot de passe du compte système Moodle : $compte_moodle_mdp
-	- Dossier d'installation racine : $dossier_moodle_racine
-	- Dossier système Moodle : $dossier_moodle_systeme
-	- Dossier data Moodle : $dossier_moodledata
+	- Compte système Moodle : $account_system_moodle_name
+	- Mot de passe du compte système Moodle : $account_system_moodle_password
+	- Dossier d'installation racine : $folder_moodle_root
+	- Dossier système Moodle : $folder_moodle_system
+	- Dossier data Moodle : $folder_moodle_data
 	- Compte root base de données : root
-	- Mot de passe du compte root base de données : $compte_db_root_mdp
-	- Nom de la base de données : $compte_moodle
-	- Utilisateur de la base de donnée : $compte_moodle
-	- Mot de passe de l'utilisateur base de donnée : $compte_db_moodle_mdp"
+	- Mot de passe du compte root base de données : $account_db_root_password
+	- Nom de la base de données : $account_system_moodle_name
+	- Utilisateur de la base de donnée : $account_system_moodle_name
+	- Mot de passe de l'utilisateur base de donnée : $account_db_moodle_password"
 
 	echo $body | mail -s "$urldusite est prêt !" $email_demandeur
 
